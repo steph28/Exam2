af4cf65c81154ae10fb22d53aa86d7e90b6461dd
steph28 <stephane.soucheyre@gmail.com>
2016-11-09 14:15:15 +0100

    ! modif fichier app.js

--- app.js
+++ app.js
@@ -1,14 +1,36 @@
 (function(){
 
+	var app = angular.module('app',[]);
 
+	app.controller('appController', function(){
+		 this.personne = object;
+	})
 
+	app.directive('header',function(){
+		return {
+			restrict : 'A',
+			templateUrl : 'partials/common/header.html'
+		}
+	});
 
+	app.directive('footer',function(){
+		return {
+			restrict : 'A',
+			templateUrl : 'partials/common/footer.html'
+		}
+		});
 
+	app.directive('home',function(){
+		return {
+			restrict : 'A',
+			templateUrl : 'partials/home/home.html'
+		}
+		});
 
-  var object = {
-    name : "Tian Bao",
-    age : 2,
-    skills : ['Manger', 'Jouer'],
-    image : 'img/panda.jpg'
-  };
-})();
+		var object = {
+			name : "Tian Bao",
+			age : 2,
+			skills : ['Manger', 'Jouer'],
+			image : 'img/panda.jpg'
+		};
+	})();
